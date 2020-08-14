3d549e4e6c913cb9329754ee63a3c9d542ff5b78
Emmanuel Hdz <emmanuelsrlok@gmail.com>
2020-08-14 00:43:46 -0500

    Seccion 12 - Google

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,8 @@ const { response } = require('express');
 const Usuario = require('../models/usuario.model');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
+
 
 const login = async(req, res = response) => {
 
@@ -44,6 +46,48 @@ const login = async(req, res = response) => {
     }
 }
 
+
+const googleSignIn = async(req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try {
+        const { name, email, picture } = await googleVerify(googleToken);
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+        if (!usuarioDB) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            });
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+
+        }
+
+        //Guardar en la base de datos
+        await usuario.save();
+
+        //Generar JWT 
+        const token = await generarJWT(usuarioDB.id);
+        res.json({
+            ok: true,
+            msg: ' Google sign in',
+            token
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            msg: ' Google sign in'
+        });
+    }
+}
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
